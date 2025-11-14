04be6f8baedfc1e498838c57209fb91544e7ab8e
Grushfav <78400561+Grushfav@users.noreply.github.com>
2025-11-14 01:03:48 -0500

    Update pagination to 5000

--- bulwark-cms-frontend/src/components/ClientsManagement.jsx
+++ bulwark-cms-frontend/src/components/ClientsManagement.jsx
@@ -927,6 +927,11 @@ const ClientsManagement = () => {
                   <SelectItem value="20">20</SelectItem>
                   <SelectItem value="50">50</SelectItem>
                   <SelectItem value="100">100</SelectItem>
+                  <SelectItem value="200">200</SelectItem>
+                  <SelectItem value="500">500</SelectItem>
+                  <SelectItem value="1000">1000</SelectItem>
+                  <SelectItem value="2000">2000</SelectItem>
+                  <SelectItem value="5000">5000</SelectItem>
                 </SelectContent>
               </Select>
               {total > 0 && (
